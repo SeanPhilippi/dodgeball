8f897406dbc7f59b598af1e8ce3567a006367f61
Sean Philippi <sean.philippi@gmail.com>
2019-05-25 18:21:54 -0500

    random button added

--- dodgeball.js
+++ dodgeball.js
@@ -4,49 +4,56 @@ const arrOfPeople = [
     name: 'Charles Young',
     age: 55,
     skillSet: 'welding',
-    placeBorn: 'Omaha, Nebraska'
+    placeBorn: 'Omaha, Nebraska',
+    isPlayer: false
   },
   {
     id: 3,
     name: 'Judy Twilight',
     age: 35,
     skillSet: 'fishing',
-    placeBorn: 'Louisville, Kentucky'
+    placeBorn: 'Louisville, Kentucky',
+    isPlayer: false
   },
   {
     id: 4,
     name: 'Cynthia Doolittle',
     age: 20,
     skillSet: 'tic tac toe',
-    placeBorn: 'Pawnee, Texas'
+    placeBorn: 'Pawnee, Texas',
+    isPlayer: false
   },
   {
     id: 5,
     name: 'John Willouby',
     age: 28,
     skillSet: 'pipe fitting',
-    placeBorn: 'New York, New York'
+    placeBorn: 'New York, New York',
+    isPlayer: false
   },
   {
     id: 6,
     name: 'Stan Honest',
     age: 20,
     skillSet: 'boom-a-rang throwing',
-    placeBorn: 'Perth, Australia'
+    placeBorn: 'Perth, Australia',
+    isPlayer: false
   },
   {
     id: 7,
     name: 'Mia Watu',
     age: 17,
     skillSet: 'acrobatics',
-    placeBorn: 'Los Angeles, California'
+    placeBorn: 'Los Angeles, California',
+    isPlayer: false
   },
   {
     id: 8,
     name: 'Walter Cole',
     age: 32,
     skillSet: 'jump rope',
-    placeBorn: 'New Orleans, Louisiana'
+    placeBorn: 'New Orleans, Louisiana',
+    isPlayer: false
   },
 ]
 
@@ -95,7 +102,6 @@ document.getElementById('new-entries').onsubmit = function addPerson(e) {
       submitMessage.classList.toggle('hidden');
     }, 2000);
     console.log(newPerson[key]);
-    console.log('false');
   }
 }
 
@@ -139,57 +145,57 @@ const listPeopleChoices = () => {
     peopleBtn.innerText = 'Hide People';
     // create people items and append to listElement
     arrOfPeople.map(person => {
-      // player element for each player
-      const li = document.createElement('li');
-      // 'make player' button
-      const button = document.createElement('button');
-      // can throw
-      const inputExpThrow = document.createElement('input')
-      inputExpThrow.setAttribute('type', 'checkbox');
-      li.appendChild(inputExpThrow);
-      li.insertBefore(document.createTextNode('can throw?'), inputExpThrow);
-      // can dodge
-      const inputExpDodge = document.createElement('input');
-      inputExpDodge.setAttribute('type', 'checkbox');
-      li.appendChild(inputExpDodge);
-      li.insertBefore(document.createTextNode('can dodge?'), inputExpDodge);
-      // has paid
-      const inputPaid = document.createElement('input');
-      inputPaid.setAttribute('type', 'checkbox');
-      li.appendChild(inputPaid);
-      li.insertBefore(document.createTextNode('has paid?'), inputPaid);
-      // is healthy
-      const inputHealthy = document.createElement('input');
-      inputHealthy.setAttribute('type', 'checkbox');
-      li.appendChild(inputHealthy);
-      li.insertBefore(document.createTextNode('is healthy?'), inputHealthy);
-      // years experience
-      const inputYears = document.createElement('input');
-      inputYears.setAttribute('type', 'checkbox');
-      li.appendChild(inputYears);
-      li.insertBefore(document.createTextNode('years of experience: '), inputYears);
+      if (!person.isPlayer) {
+        // player element for each player
+        const li = document.createElement('li');
+        // 'make player' button
+        const button = document.createElement('button');
+        // can throw
+        const inputExpThrow = document.createElement('input')
+        inputExpThrow.setAttribute('type', 'checkbox');
+        li.appendChild(inputExpThrow);
+        li.insertBefore(document.createTextNode('can throw?'), inputExpThrow);
+        // can dodge
+        const inputExpDodge = document.createElement('input');
+        inputExpDodge.setAttribute('type', 'checkbox');
+        li.appendChild(inputExpDodge);
+        li.insertBefore(document.createTextNode('can dodge?'), inputExpDodge);
+        // has paid
+        const inputPaid = document.createElement('input');
+        inputPaid.setAttribute('type', 'checkbox');
+        li.appendChild(inputPaid);
+        li.insertBefore(document.createTextNode('has paid?'), inputPaid);
+        // is healthy
+        const inputHealthy = document.createElement('input');
+        inputHealthy.setAttribute('type', 'checkbox');
+        li.appendChild(inputHealthy);
+        li.insertBefore(document.createTextNode('is healthy?'), inputHealthy);
+        // years experience
+        const inputYears = document.createElement('input');
+        inputYears.setAttribute('type', 'checkbox');
+        li.appendChild(inputYears);
+        li.insertBefore(document.createTextNode('years of experience: '), inputYears);
 
-      button.innerHTML = 'Make Player';
-      button.addEventListener('click', () => {
+        button.innerHTML = 'Make Player';
+        button.addEventListener('click', () => {
           makePlayer(person.id);
-        }
-      );
+        });
 
-      li.appendChild(button);
-      li.appendChild(document.createTextNode(`name: ${person.name}
-      skill: ${person.skillSet}`));
-      li.setAttribute('id', person.id);
-      li.setAttribute('class', 'person')
-      listElement.append(li);
-    });
-      // remove people items that have their button clicked.
+        li.appendChild(button);
+        li.appendChild(document.createTextNode(`name: ${person.name} skill: ${person.skillSet}`));
+        li.setAttribute('id', person.id);
+        li.setAttribute('class', 'person')
+        listElement.append(li);
+      }
+    })
+    // remove people items that have their button clicked.
     listElement.addEventListener('click', e => {
       if (e.target.nodeName === 'BUTTON') {
         // targeting parent of button clicked (li element)
         e.target.parentNode.remove();
-        console.log('e', e, 'e.target', e.target);
+        // console.log('e', e, 'e.target', e.target);
       }
-    });
+    })
   }
 };
 
@@ -198,37 +204,42 @@ const deleteChildren = () => {
   while (peopleList.hasChildNodes()) {
     peopleList.removeChild(peopleList.firstChild);
   };
-  document.getElementById('button').innerText = 'List People'
+  document.getElementById('button').innerText = 'List People';
 }
 
 const makePlayer = id => {
   const playerList = document.getElementById('players');
   const newPlayer = arrOfPeople.find(player => player.id === id);
   const createdPlayer = new DodgeBallPlayer(newPlayer.name, newPlayer.skillSet, newPlayer.id);
-  console.log('createdPlayer', createdPlayer)
+  console.log('createdPlayer', createdPlayer);
   listOfPlayers.push(createdPlayer);
   const li = document.createElement('li');
   const blueButton = document.createElement('button');
   blueButton.innerHTML = 'Make Blue Teammate';
   const redButton = document.createElement('button');
   redButton.innerHTML = 'Make Red Teammate';
-  blueButton.addEventListener('click', (e) => makeBlueTeammate(newPlayer.id, e));
-  redButton.addEventListener('click', (e) => makeRedTeammate(newPlayer.id, e));
+  const randomButton = document.createElement('button');
+  randomButton.innerHTML = 'Random Team';
+  blueButton.addEventListener('click', e => makeBlueTeammate(newPlayer.id, e));
+  redButton.addEventListener('click', e => makeRedTeammate(newPlayer.id, e));
+  randomButton.addEventListener('click', e => assignRandom(newPlayer.id, e))
   li.appendChild(document.createTextNode(`name: ${newPlayer.name} | id: ${newPlayer.id}`))
   li.appendChild(blueButton);
   li.appendChild(redButton);
+  li.appendChild(randomButton);
   playerList.append(li);
-
+  // toggle isPlayer prop to true so they are not shown by List People button anymore
+  arrOfPeople.find(person => person.id === id).isPlayer = true;
+  console.log('arrOfPeople', arrOfPeople)
 };
 
 const makeBlueTeammate = (id, e) => {
   const blueList = document.getElementById('blue');
   const li = document.createElement('li');
   const newTeammate = listOfPlayers.find(player => player.id === id);
-  const newBlue = new BlueTeammate(newTeammate.name, newTeammate.skillSet, newTeammate.id, 'balls', 'blue');
+  const newBlue = new BlueTeammate(newTeammate.name, newTeammate.skillSet, newTeammate.id, 'dolphin', 'blue');
   li.appendChild(document.createTextNode(`${newBlue.name} - ${newBlue.color} - ${newBlue.mascot}`));
   blueList.append(li);
-  console.log('this', this);
   e.target.parentNode.remove();
 }
 
@@ -240,4 +251,13 @@ const makeRedTeammate = (id, e) => {
   li.appendChild(document.createTextNode(`${newRed.name} - ${newRed.color} - ${newRed.mascot}`));
   redList.append(li);
   e.target.parentNode.remove();
+}
+
+const assignRandom = (id, e) => {
+  const getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  // get random #, 0 or 1
+  const num = getRandomInt(2);
+  return num === 0 ? makeBlueTeammate(id, e) : makeRedTeammate(id, e);
 }
